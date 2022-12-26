92ddd975b97763b582c7f975bfbfe371ec3b3878
omartins-zs <gabrielmatheus209@gmail.com>
2022-12-26 09:55:28 -0300

    Adicionando apelido e Metodo de Update Usuarios

--- application/controllers/Pessoa.php
+++ application/controllers/Pessoa.php
@@ -14,12 +14,15 @@ class Pessoa extends CI_Controller
 
 	public function index()
 	{
-		$this->load->view('pessoa/cadastra_pessoa');
+		$this->load->view('pessoa/pessoa');
 	}
+
 	public function cadastrar()
 	{
 		// Pessoa
 		$param['nome'] = $this->input->post('nome');
+		$param['sobrenome'] = $this->input->post('sobrenome');
+		$param['apelido'] = $this->input->post('apelido');
 		$param['documento'] = $this->input->post('documento');
 		$param['email'] = $this->input->post('email');
 		$param['data_nasc'] = $this->input->post('data_nasc');
@@ -34,5 +37,17 @@ class Pessoa extends CI_Controller
 			$paramUsu['idPessoa'] = $lastId;
 			$this->Usuario_model->insertUsuario($paramUsu);
 		}
+	}
+	public function atualizar()
+	{
+		// Pessoa
+		$param['nome'] = $this->input->post('nome');
+		$param['sobrenome'] = $this->input->post('sobrenome');
+		$param['apelido'] = $this->input->post('apelido');
+		$param['email'] = $this->input->post('email');
+
+		$this->Pessoa_model->atualizarDados($param);
+
+		redirect('pessoa');
 	}
 }
--- application/models/Pessoa_model.php
+++ application/models/Pessoa_model.php
@@ -11,13 +11,30 @@ class Pessoa_model extends CI_Model
     {
         $campos = array(
             'nome' =>    $param['nome'],
+            'sobrenome' =>    $param['sobrenome'],
+            'apelido' =>    $param['apelido'],
             'documento' =>    $param['documento'],
             'email' =>    $param['email'],
-            'data_nasc' =>   date('Y-m-d', strtotime(str_replace('/','-', $param['data_nasc'])))
+            'data_nasc' =>   date('Y-m-d', strtotime(str_replace('/', '-', $param['data_nasc'])))
         );
 
         $this->db->insert('pessoas', $campos);
 
         return $this->db->insert_id();
+    }
+
+    public function atualizarDados($param)
+    {
+        $campos = array(
+            'nome' =>    $param['nome'],
+            'sobrenome' =>    $param['sobrenome'],
+            'apelido' =>    $param['apelido'],
+            'email' =>    $param['email'],
+        );
+
+        $this->db->update('pessoas', $campos);
+        $this->db->where('idPessoa', $this->session->userdata('s_idPesoa'));
+
+        return 1;
     }
 }
