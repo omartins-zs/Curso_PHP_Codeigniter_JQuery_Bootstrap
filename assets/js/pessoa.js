2477302794a1ec1d0692760921a24053816e0597
omartins-zs <gabrielmatheus209@gmail.com>
2022-12-27 09:41:15 -0300

    Teste do Select de Cidades, Btn Get pessoas Alert

--- assets/js/pessoa.js
+++ assets/js/pessoa.js
@@ -3,5 +3,27 @@ $.post(baseUrl + "cidade/getCidades",
         situacao: 1,
     },
     function (data) {
-        alert(data);
+        var c = JSON.parse(data);
+        $.each(c, function (i, item) {
+            $('#selectCidades').append('<option value="' + item.idCidade + '">' + item.cidade + ' </option>');
+        })
     });
+$('#selectCidades').change(function () {
+    $('#selectCidades option:selected').each(function () {
+        var id = $('#selectCidades').val();
+        alert(id);
+
+    })
+})
+
+$('#btnGetPessoas').click(function(){
+    // alert('entrou no button');
+    $.post(baseUrl + "pessoa/getPessoas",
+        function (data) {
+            alert(data);
+
+            // var c = JSON.parse(data);
+            // $.each(c, function (i, item) {
+            //     $('#selectCidades').append('<option value="' + item.idCidade + '">' + item.cidade + ' </option>');
+        });
+});
