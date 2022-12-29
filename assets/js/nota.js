a171b7e1fa290cfae2bbc4050b42333d2fd4a576
omartins-zs <gabrielmatheus209@gmail.com>
2022-12-29 12:48:22 -0300

    Criando Operaçoes do Input dentro da Tabela

--- assets/js/nota.js
+++ assets/js/nota.js
@@ -1,30 +1,46 @@
 $.post(baseUrl + "nota/getNotas",
 	function (data) {
 		var obj = JSON.parse(data);
-		var estilo = "background-color: transparent;border: 0px; outline: none; text-align: center;width: 100 %";
+		var style = "background-color: transparent;border: 0px; outline: none; text-align: center;width: 100%";
 		$.each(obj, function (i, item) {
-			$('#tableNotas').append(
-				'<tr>' +
+			$('#tableNotas tbody').append(
+				'<tr class="linhaNotas">' +
 				' <td> 1</td>' +
-				' <td>' + item.aluno + '</td>' +
-				' <td><input type="text" value="' + item.a + '" style="' + estilo + '" maxlenght="2" class="nota1></td>' +
-				' <td><input type="text" value="' + item.b + '" style="' + estilo + '" maxlenght="2" class="nota2></td>' +
-				' <td><input type="text" value="' + item.c + '" style="' + estilo + '" maxlenght="2" class="nota3></td>' +
-				' <td><input type="text" value="' + item.d + '" style="' + estilo + '" maxlenght="2" class="nota4></td>' +
-				' <td><input type="text" value="' + item.notaFinal + '" style="' + estilo + '" maxlenght="2" class="notaFinal></td>' +
+				' <td><div class="aluno" id="' + item.idPessoa + '"></div>' + item.aluno + '</td>' +
+				' <td><input type="text" value="' + item.a + '" style="' + style + '" maxlength="2" class="nota1"></td>' +
+				' <td><input type="text" value="' + item.b + '" style="' + style + '" maxlength="2" class="nota2"></td>' +
+				' <td><input type="text" value="' + item.c + '" style="' + style + '" maxlength="2" class="nota3"></td>' +
+				' <td><input type="text" value="' + item.d + '" style="' + style + '" maxlength="2" class="nota4"></td>' +
+				' <td><input type="text" value="' + item.notaFinal + '" style="' + style + '" maxlength="2" class="notaFinal"></td>' +
 				'</tr>'
 			);
 		});
 		$('input[type=text]').focus(function () {
 			$(this).select();
 		});
-		$('input[type=text]').focusot(function () {
+
+		$('input[type=text]').focusout(function () {
 			if ($(this).val() > 20) {
+				alert('Este valor é invalido!');
 				$(this).focus();
 				$(this).select();
 			}
-		})
+		});
+
+		$('#tableNotas .nota4').keyup(function () {
+			var i = $('.nota4').index(this);
+
+			var n1 = $('.nota1:eq(' + i + ')').val();
+			var n2 = $('.nota2:eq(' + i + ')').val();
+			var n3 = $('.nota3:eq(' + i + ')').val();
+
+			// var n4 = $('.nota1:eq('+i+')').val();
+			var media = (parseFloat(n1) + parseFloat(n2) + parseFloat(n3) + parseFloat($(this).val())) / 4;
+
+			$('.notaFinal:eq(' + i + ')').val(media);
+		});
 	});
+
 
             // alert(data);
             // // situacao: 1,
