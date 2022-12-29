b5cccd40c133192f7e3eba319f10b94a5f08c77a
omartins-zs <gabrielmatheus209@gmail.com>
2022-12-29 14:52:04 -0300

    Adicionando Link de Notas e Cadastrar Pessoa

--- application/views/templates/menu.php
+++ application/views/templates/menu.php
@@ -28,21 +28,21 @@
               <li class="header">MAIN NAVIGATION</li>
               <li class="active treeview">
                   <a href="#">
-                      <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+                      <i class="fa fa-dashboard"></i> <span>Pessoas</span> <i class="fa fa-angle-left pull-right"></i>
                   </a>
                   <ul class="treeview-menu">
-                      <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-                      <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                      <li class="active"><a href="<?= base_url(); ?>pessoa"><i class="fa fa-circle-o"></i> Cadastrar Pessoa</a></li>
+                      <li><a href="<?= base_url(); ?>cidade"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
                   </ul>
               </li>
               <li class="treeview">
                   <a href="#">
                       <i class="fa fa-files-o"></i>
-                      <span>Layout Options</span>
+                      <span>Novas opções</span>
                       <span class="label label-primary pull-right">4</span>
                   </a>
                   <ul class="treeview-menu">
-                      <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+                      <li><a href="<?= base_url(); ?>nota"><i class="fa fa-circle-o"></i> Inserir notas</a></li>
                       <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
                       <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
                       <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
@@ -167,5 +167,5 @@
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    
-  <div class="content">
+
+      <div class="content">
