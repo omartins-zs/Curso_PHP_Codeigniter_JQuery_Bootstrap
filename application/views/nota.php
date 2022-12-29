d6550140a19ad67f2e1aaf6a1632505ee58c0530
omartins-zs <gabrielmatheus209@gmail.com>
2022-12-29 10:20:08 -0300

    Add View do Teste de busca com AJAX

---
+++ application/views/nota.php
@@ -0,0 +1,25 @@
+<div class="col-sm-5">
+
+	<div class="box box-primary">
+
+		<table id="tableNotas" class="table table-bordered table-striped">
+			<tr>
+				<th style="width: 10px">#</th>
+				<th>N°</th>
+				<th>Aluno</th>
+				<th>1B</th>
+				<th>2B</th>
+				<th>3B</th>
+				<th>4B</th>
+				<th>Final</th>
+			</tr>
+		</table>
+	</div>
+</div>
+<div class="col-sm-1">
+	<button type="button" class="btn btn-block btn-primary">Gravar</button>
+</div>
+
+<script type="text/javascript">
+	var baseUrl = "<?= base_url(); ?>";
+</script>
