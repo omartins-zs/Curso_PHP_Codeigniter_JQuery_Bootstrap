38d814020e981f60f4aea95baabebd2684759064
omartins-zs <gabrielmatheus209@gmail.com>
2023-01-11 09:23:31 -0300

    Criando Card e Icon com Imagem e dados da Cidade

---
+++ application/views/nota copy.php
@@ -0,0 +1,56 @@
+<!-- USERS LIST -->
+<div class="box box-danger">
+	<div class="box-header with-border">
+		<h3 class="box-title">Latest Members</h3>
+
+		<div class="box-tools pull-right">
+			<span class="label label-danger">8 New Members</span>
+			<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+			</button>
+			<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+			</button>
+		</div>
+	</div>
+
+
+	<!-- /.box-header -->
+	<div class="box-body no-padding">
+		<ul class="users-list clearfix" id="listCidades">
+
+
+
+		</ul>
+	</div>
+
+	<div class="box box-primary">
+
+		<table id="tableNotas" class="table table-bordered table-striped">
+			<tr>
+				<th style="width: 10%">#</th>
+				<th style="width: 40%">Aluno</th>
+				<th style="width: 10%">1B</th>
+				<th style="width: 10%">2B</th>
+				<th style="width: 10%">3B</th>
+				<th style="width: 10%">4B</th>
+				<th style="width: 10%">Final</th>
+			</tr>
+		</table>
+	</div>
+</div>
+
+<!-- /.box-body -->
+<div class="box-footer text-center">
+	<a href="javascript:void(0)" class="uppercase">View All Users</a>
+</div>
+<!-- /.box-footer -->
+<button type="button" class="btn btn-block btn-primary" id="btnGravar">Gravar</button>
+</div>
+<!-- /.box -->
+</div>
+
+
+
+
+<script type="text/javascript">
+	var baseUrl = "<?= base_url(); ?>";
+</script>
