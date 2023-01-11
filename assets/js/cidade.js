47ab0f86fa7ee390ec4e3c8f0702b9523a583cdf
omartins-zs <gabrielmatheus209@gmail.com>
2023-01-11 09:21:54 -0300

    Criando JS com Metodos para o Cidades

---
+++ assets/js/cidade.js
@@ -0,0 +1,41 @@
+$.post(
+	baseUrl + "cidade/getCidades",
+	{
+		situacao: 1,
+	},
+	function (data) {
+		var obj = JSON.parse(data);
+		var estilo =
+			"width:100px;height:100px;-moz-border-radius:50%;-webkit-border-radius:50%;border-radius:50%;background:#5CB85C";
+		output = "";
+		$.each(obj, function (i, item) {
+			output +=
+				" <li>" +
+				' <div style="' +estilo +'" class="">' +
+				'<input type="text" style="width="50%" value="' +item.cidade +'" class=""clsNomeCidade">' +
+				'<a class="users-list-name" href="#">' + item.cidade + "</a>" +
+				'<span class="users-list-date">' + item.idCidade + "</span>" +
+				" </li>";
+		});
+
+		$("#listCidades").html(output);
+		$("#listCidades .clsCidade").click(function () {
+			var i = $(".clsCidade").index(this);
+			var nc = $(".clsNomeCidade:eq(" + i + ")").val();
+			alert(i + nc);
+		});
+	}
+);
+
+$("#btnGravar").click(function () {
+	var i = 0;
+	$("#listCidades .clsNomeCidade").each(function () {
+		var nc = $(".clsNomeCidade:eq(" + i + ")").val();
+		i++;
+		alert(nc);
+
+		$.post("gravarCidade", function (data) {
+			alert("Foi gravado corretamente");
+		});
+	});
+});
